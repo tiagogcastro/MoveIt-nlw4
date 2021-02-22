90e110886f1d4edbe4d7095f1a5ddfa286e2693a
Tiaguin061 <tiaguin180@gmail.com>
2021-02-22 15:53:06 -0300

    Finalized first day

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+
+export function ExperienceBar() {
+  return (
+    <>
+    <header className="experience-bar">
+      <span>300 xp</span>
+      <div>
+        <div style={{width: '50%'}}>
+          <span className="current-experience" style={{left: '50%'}}>
+            300 xp
+          </span>
+        </div>
+      </div>
+      <span>600 xp</span>
+    </header>
+    </>
+  )
+}
